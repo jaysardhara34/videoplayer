9f0a613eabc4129640c9da02462bc1fcecb8f3e6
jaysardhara34 <jack.sardhara01@gamil.com>
2022-09-21 15:00:27 +0530

    splascreen

--- lib/screen/video/provider/videoProvider.dart
+++ lib/screen/video/provider/videoProvider.dart
@@ -3,6 +3,7 @@ import 'package:videoplayer/screen/video/modal/videoModal.dart';
 
 class VideoProvider extends ChangeNotifier {
   bool isPlay = true;
+  int i = 0;
 
   void change() {
     isPlay = !isPlay;
@@ -10,12 +11,30 @@ class VideoProvider extends ChangeNotifier {
   }
 
   List<VideoModal> l1 = [
-    VideoModal(Videos: 'assets/videos/tanjiro.mp4',name: "Tanjiro - HinokamiKagura",informaton: 'Tanjiro uses HinokamiKagura For First time.'),
-    VideoModal(Videos: 'assets/videos/sukuna.mp4',name: "Sukuna Edit AMV",informaton: 'Sukuna AMV Edit Super Cool.'),
-    VideoModal(Videos: 'assets/videos/zenitsu.mp4',name: "Zenitsu - Thunder Edit",informaton: 'Zenitsu First time Sleep Thunder breathing.'),
-    VideoModal(Videos: 'assets/videos/rengoku.mp4',name: "Rengoku vs Akaza Edit",informaton: 'Rengoku vs Akaza AMV Edit looks Really Amazing.'),
-    VideoModal(Videos: 'assets/videos/gojo.mp4',name: "Gojo-Edit AMV",informaton: 'Gojo Super cool Anime Video Edit.'),
-    VideoModal(Videos: 'assets/videos/madara.mp4',name: "Madara - Gokakyu no Jutsu",informaton: 'Ghost of uchiha Uses his powerful Katon.'),
+    VideoModal(
+        Videos: 'assets/videos/tanjiro.mp4',
+        name: "Tanjiro - HinokamiKagura",
+        informaton: 'Tanjiro uses HinokamiKagura For First time.'),
+    VideoModal(
+        Videos: 'assets/videos/sukuna.mp4',
+        name: "Sukuna Edit AMV",
+        informaton: 'Sukuna AMV Edit Super Cool.'),
+    VideoModal(
+        Videos: 'assets/videos/zenitsu.mp4',
+        name: "Zenitsu - Thunder Edit",
+        informaton: 'Zenitsu First time Sleep Thunder breathing.'),
+    VideoModal(
+        Videos: 'assets/videos/rengoku.mp4',
+        name: "Rengoku vs Akaza Edit",
+        informaton: 'Rengoku vs Akaza AMV Edit looks Really Amazing.'),
+    VideoModal(
+        Videos: 'assets/videos/gojo.mp4',
+        name: "Gojo-Edit AMV",
+        informaton: 'Gojo Super cool Anime Video Edit.'),
+    VideoModal(
+        Videos: 'assets/videos/madara.mp4',
+        name: "Madara - Gokakyu no Jutsu",
+        informaton: 'Ghost of uchiha Uses his powerful Katon.'),
   ];
   List<VideoModal> img = [
     VideoModal(image: 'assets/images/tanjiro.jpg'),
@@ -25,6 +44,25 @@ class VideoProvider extends ChangeNotifier {
     VideoModal(image: 'assets/images/gojo.jpg'),
     VideoModal(image: 'assets/images/madara.jpg'),
   ];
+  List<VideoModal> sliderimg = [
+    VideoModal(image: 'assets/images/1.png'),
+    VideoModal(image: 'assets/images/2.png'),
+    VideoModal(image: 'assets/images/3.jpg'),
+    VideoModal(image: 'assets/images/Rengoku.jpg'),
+    VideoModal(image: 'assets/images/4.jpg'),
+    VideoModal(image: 'assets/images/5.jpg'),
+    VideoModal(image: 'assets/images/6.jpg'),
+    VideoModal(image: 'assets/images/8.jpg'),
+    VideoModal(image: 'assets/images/12jpg'),
+    VideoModal(image: 'assets/images/13.jpg'),
+    VideoModal(image: 'assets/images/14.jpg'),
+    VideoModal(image: 'assets/images/15.jpg'),
+  ];
   List data2 = [];
   VideoModal? datapickk;
+
+  void changepi(int index) {
+    i = index;
+    notifyListeners();
+  }
 }
--- lib/screen/video/view/videoScreen.dart
+++ lib/screen/video/view/videoScreen.dart
@@ -1,7 +1,7 @@
-import 'package:chewie/chewie.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:video_player/video_player.dart';
 import 'package:videoplayer/screen/video/modal/videoModal.dart';
 import 'package:videoplayer/screen/video/provider/videoProvider.dart';
 
@@ -21,37 +21,79 @@ class _VideoScreenState extends State<VideoScreen> {
     protrue = Provider.of<VideoProvider>(context, listen: true);
     profalse = Provider.of<VideoProvider>(context, listen: false);
     return SafeArea(
-      child: Scaffold(backgroundColor: Color(0xff4f4f4f),
-        body: ListView.builder(
-          itemCount: profalse!.l1.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                profalse!.datapickk =
-                    VideoModal(Videos: profalse!.l1[index].Videos,name: profalse!.l1[index].name,informaton: profalse!.l1[index].informaton,);
-                Navigator.pushNamed(context, 'play');
+      child: Scaffold(
+        backgroundColor: Color(0xff4f4f4f),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            CarouselSlider.builder(
+              itemCount: profalse!.img.length,
+              itemBuilder: (context, index, _) {
+                return Image.asset(
+                  '${profalse!.sliderimg[index].image}',
+                  fit: BoxFit.cover,
+                );
               },
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "${index + 1 }. ${profalse!.l1[index].name}",
-                    style: TextStyle(fontSize: 20, color: Color(0xffffffff)),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    height: 205,
-                    child: Image.asset("${profalse!.img[index].image}"),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
+              options: CarouselOptions(
+                onPageChanged: (index ,_){
+                  profalse!.changepi(index);
+                },
+                  autoPlay: true,
+                  enableInfiniteScroll: true,
+                  enlargeCenterPage: true),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            DotsIndicator(
+              dotsCount: protrue!.sliderimg.length,
+              position: protrue!.i.toDouble(),
+              decorator: DotsDecorator(
+                  activeColor: Color(0xff000000), color: Color(0xff9f9f9f)),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: profalse!.l1.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      profalse!.datapickk = VideoModal(
+                        Videos: profalse!.l1[index].Videos,
+                        name: profalse!.l1[index].name,
+                        informaton: profalse!.l1[index].informaton,
+                      );
+                      Navigator.pushNamed(context, 'play');
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${index + 1}. ${profalse!.l1[index].name}",
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xffffffff)),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          height: 205,
+                          child: Image.asset("${profalse!.img[index].image}"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
